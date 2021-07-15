914aa3a36e218585049a0c7aebb58f3e4cdb7776
Samuel Asher Rivello <74617375+srivelloBeamable@users.noreply.github.com>
2021-07-15 12:39:14 -0600

    Add MyADBMicroserviceExample

---
+++ client/Assets/Examples/Runtime/Features/Microservices/MyADBMicroserviceExample/Scripts/Client/MyADBMicroserviceExample.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using Beamable.Server.Clients;
+
+namespace Beamable.Examples.Features.Microservices.MyADBMicroserviceExample
+{
+    /// <summary>
+    /// Demonstrates <see cref="Microservices"/>.
+    /// </summary>
+    public class MyADBMicroserviceExample : MonoBehaviour
+    {
+        //  Properties  -----------------------------------
+        
+        //  Fields  ---------------------------------------
+        private MyADBMicroserviceClient  _myAdbMicroserviceClient;
+        
+        //  Unity Methods  --------------------------------
+
+        protected void Start()
+        {
+            Debug.Log("Start() Instructions...\n" + 
+            "* Complete docker setup per https://docs.beamable.com/docs/microservices-feature\n" +
+            "* Start the server per https://docs.beamable.com/docs/microservices-feature\n" +
+            "* Play This Scene\n" + 
+            "* Enjoy!\n\n\n");
+            
+            SetupBeamable();
+        }
+        
+        //  Methods  --------------------------------------
+        private async void SetupBeamable()
+        {
+            var beamableAPI = await Beamable.API.Instance;
+
+            Debug.Log($"beamableAPI.User.id = {beamableAPI.User.id}");
+            
+            _myAdbMicroserviceClient = new MyADBMicroserviceClient();
+            
+            // #1 - Call Microservice
+            //int playerLevel = await _myAdbMicroserviceClient.GetPlayerLevel();
+                
+            // #2 - Result = 1
+           // Debug.Log ($"GetPlayerLevel() Result = {playerLevel}");
+        }
+    }
+}

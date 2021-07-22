5c1dc6822bf92ca480a600336ed561559c61a542
Samuel Asher Rivello <74617375+srivelloBeamable@users.noreply.github.com>
2021-07-22 09:50:02 -0600

    Added TimedRewardsExample

---
+++ client/Assets/Examples/Runtime/Features/Microservices/TimedRewardsMicroserviceExample/Scripts/Client/TimedRewardsMicroserviceExample.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using Beamable.Server.Clients;
+
+namespace Beamable.Examples.Features.Microservices.TimedRewardsMicroserviceExample
+{
+    /// <summary>
+    /// Demonstrates <see cref="Microservices"/>.
+    /// </summary>
+    public class TimedRewardsMicroserviceExample : MonoBehaviour
+    {
+        //  Properties  -----------------------------------
+        
+        //  Fields  ---------------------------------------
+        [SerializeField] 
+        private TimeRewardRef _timeRewardRef = null;
+        
+        private TimedRewardServiceClient  _timedRewardServiceClient;
+        
+        //  Unity Methods  --------------------------------
+
+        protected void Start()
+        {
+            Debug.Log("Start() Instructions...\n" + 
+            "* Complete docker setup per https://docs.beamable.com/docs/microservices-feature\n" +
+            "* Start the server per https://docs.beamable.com/docs/microservices-feature\n" +
+            "* Play This Scene\n" + 
+            "* Enjoy!\n\n\n");
+            
+            SetupBeamable();
+        }
+        
+        //  Methods  --------------------------------------
+        private async void SetupBeamable()
+        {
+            var beamableAPI = await Beamable.API.Instance;
+
+            Debug.Log($"beamableAPI.User.id = {beamableAPI.User.id}");
+            
+            _timedRewardServiceClient = new TimedRewardServiceClient();
+            
+            // #1 - Call Microservice
+            bool isSuccess = await _timedRewardServiceClient.Claim(_timeRewardRef);
+                
+            // #2 - Result = true
+            Debug.Log ($"AddMyValues() isSuccess = {isSuccess}");
+        }
+    }
+}
---
+++ client/Assets/Examples/Runtime/Features/Microservices/TimedRewardsMicroserviceExample/Scripts/Server/readme.txt
@@ -0,0 +1,3 @@
+Unlike other Microservices in this project, the source for the TimedRewardsService was not created in this project.
+
+It is imported. See https://docs.beamable.com/docs/distributing-a-microservice

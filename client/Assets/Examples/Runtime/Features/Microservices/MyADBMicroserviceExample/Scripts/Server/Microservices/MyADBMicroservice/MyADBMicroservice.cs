0f1743cfd9e8e7cdfe766f2a5c0e0355ff6f33f9
Samuel Asher Rivello <74617375+srivelloBeamable@users.noreply.github.com>
2021-12-01 15:17:44 -0600

    Remove TimedRewardsMicroserviceExample

--- client/Assets/Examples/Runtime/Features/Microservices/MyADBMicroserviceExample/Scripts/Server/Microservices/MyADBMicroservice/MyADBMicroservice.cs
+++ client/Assets/Examples/Runtime/Features/Microservices/MyADBMicroserviceExample/Scripts/Server/Microservices/MyADBMicroservice/MyADBMicroservice.cs
@@ -15,25 +15,25 @@ namespace Beamable.Server.MyADBMicroservice.MyADBMicroserviceExample
       {
          // TODO: Create an account with https://aws.amazon.com/dynamodb/ 
          // TODO: And add your keys here
-         string accessKey = "replace-with-your-access-key";
-         string secretKey = "replace-with-your-secret-key";
+         //string accessKey = "replace-with-your-access-key";
+         //string secretKey = "replace-with-your-secret-key";
          
          try
          {
-            // Credentials
-            var credentials = new BasicAWSCredentials(accessKey, secretKey);
-            
-            // Configuration
-            var config = new AmazonDynamoDBConfig()
-            {
-               RegionEndpoint = RegionEndpoint.USWest2
-            };
-            
-            // Client
-            AmazonDynamoDBClient client = 
-               new AmazonDynamoDBClient(credentials, config);
-            
-            Debug.Log($"CallADB() Success!");
+            // // Credentials
+            // var credentials = new BasicAWSCredentials(accessKey, secretKey);
+            //
+            // // Configuration
+            // var config = new AmazonDynamoDBConfig()
+            // {
+            //    RegionEndpoint = RegionEndpoint.USWest2
+            // };
+            //
+            // // Client
+            // AmazonDynamoDBClient client = 
+            //    new AmazonDynamoDBClient(credentials, config);
+            //
+            // Debug.Log($"CallADB() Success!");
             
          }
          catch (Exception e)
--- client/Assets/Examples/Runtime/Features/Microservices/TimedRewardsMicroserviceExample/Scripts/Server/readme.txt
+++
@@ -1,3 +0,0 @@
-Unlike other Microservices in this project, the source for the TimedRewardsService was not created in this project.
-
-It is imported. See https://docs.beamable.com/docs/guide-distributing-a-microservice
